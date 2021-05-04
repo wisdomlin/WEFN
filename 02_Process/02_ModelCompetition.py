91e787a0393379779fafc25e6431e95b6482ec90
Professor Aang <beck1017@hotmail.com>
2021-05-04 21:50:55 +0800

    Add 02_ModelCompetition.py

--- 02_Process/02_ModelCompetition.py
+++ 02_Process/02_ModelCompetition.py
@@ -1,11 +1,10 @@
-# compare algorithms
+# make predictions
 from pandas import read_csv
-from matplotlib import pyplot
 
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection._split import RepeatedStratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
 
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
@@ -21,14 +20,20 @@ dataset = read_csv(filepath, names=names)
 
 # Split-out validation dataset
 array = dataset.values
-# Features (Column 0~3)
-X = array[:,0:4]	
-# Label (Column 4)
-y = array[:,4]		
-# split train/test set, and stratify by label
+X = array[:,0:4]
+y = array[:,4]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=1, stratify=y)
 
-# Spot Check Algorithms
+# # Make predictions on validation dataset
+# model = SVC(gamma='auto')
+# model.fit(X_train, y_train)
+# predictions = model.predict(X_test)
+
+# # Evaluate predictions
+# print(accuracy_score(y_test, predictions))
+# print(confusion_matrix(y_test, predictions))
+# print(classification_report(y_test, predictions))
+
 models = []
 models.append(('LR', LogisticRegression(solver='liblinear', multi_class='ovr')))
 models.append(('LDA', LinearDiscriminantAnalysis()))
@@ -37,19 +42,17 @@ models.append(('CART', DecisionTreeClassifier()))
 models.append(('NB', GaussianNB()))
 models.append(('SVM', SVC(gamma='auto')))
 
-# evaluate each model in turn
+# train and test each model in turn
 results = []
 names = []
 for name, model in models:
-	kfold = RepeatedStratifiedKFold(n_splits=4, n_repeats=3, random_state=36851234)
-	cv_results = cross_val_score(model, X_train, y_train, cv=kfold, scoring='accuracy')
-	results.append(cv_results)
+	model.fit(X_train, y_train)
+	predictions = model.predict(X_test)
+	results.append(predictions)
 	names.append(name)
-	print('%s: %f (%f)' % (name, cv_results.mean(), cv_results.std()))
-
-# Compare Algorithms (Model Selection)
-pyplot.boxplot(results, labels=names)
-pyplot.title('Algorithm Comparison')
-pyplot.show()
+	print('model: ' + name)
+	print(accuracy_score(y_test, predictions))
+	print(confusion_matrix(y_test, predictions))
+	print(classification_report(y_test, predictions))
 
 
