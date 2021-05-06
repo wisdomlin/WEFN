06214020a8d114f000d283c042fee7097b730a80
Professor Aang <beck1017@hotmail.com>
2021-05-06 16:20:44 +0800

    code cleanup

--- 02_Process/02_ModelCompetition.py
+++ 02_Process/02_ModelCompetition.py
@@ -1,5 +1,4 @@
 from pandas import read_csv
-import numpy as np
 
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
@@ -26,39 +25,7 @@ df = read_csv(filepath, header=None, skiprows=1, names=names, encoding='utf8')
 # split train/test set
 train, test = train_test_split(df, random_state=42, test_size=0.20, shuffle=True)
 
-#  from 29 to 59 for backup
-# # define model
-# model = LogisticRegression(solver ='lbfgs', max_iter = 400)
-# # define OvR strategy
-# ovr = OneVsRestClassifier(model)
-
-# # multilable classification
-# y_trues = []
-# y_preds = []
-# for label in labels:
-#     # print('... Processing label: {}'.format(label))
-#     X_train = train[features].values	
-#     y_train = train[label].values
-#     X_test = test[features].values
-#     y_test = test[label].values
-#     # fit model    
-#     ovr.fit(X_train, y_train)    
-#     # make predictions
-#     y_pred = ovr.predict(X_test)
-#     # print('ytrue is {}'.format(test[label].values))
-#     y_preds.append(y_pred)
-#     # print('yhat is {}'.format(yhat))
-#     y_trues.append(y_test)
-#     # evaluate predictions
-#     print('Test accuracy is {}'.format(accuracy_score(y_test, y_pred)))
-
-# # print('y_trues is {}'.format(y_trues))
-# # print('y_preds is {}'.format(y_preds))
-# print('multilable classification accuracy_score is {}'.format(accuracy_score(y_trues, y_preds)))
-# # print(confusion_matrix(y_trues, y_preds))
-# # print(classification_report(y_trues, y_preds))
-
-#-----------------------------------------------------------------------------
+# define model
 models = []
 models.append(('LR', LogisticRegression(solver ='lbfgs', max_iter = 400)))
 models.append(('LDA', LinearDiscriminantAnalysis()))
@@ -68,15 +35,10 @@ models.append(('NB', GaussianNB()))
 models.append(('SVM', SVC(gamma='auto')))
 
 # train and test each model in turn
-results = []
-names = []
 for name, model in models:    
     print('model: ' + name)
-    names.append(name)
-	# model.fit(X_train, y_train)
-	# predictions = model.predict(X_test)
+    # define OvR strategy
     ovr = OneVsRestClassifier(model)
-    
     # multilable classification
     y_trues = []
     y_preds = []
@@ -96,7 +58,7 @@ for name, model in models:
         y_trues.append(y_test)
         # print('Test accuracy is {}'.format(accuracy_score(y_test, y_pred)))
 
-    # Multilabel Evaluation Metrics
+    # multilabel evaluation metrics
     # print('Subset accuracy (exact match ratio) is {}'.format(accuracy_score(y_trues, y_preds)))
     print('Hamming score (label-based accuracy) is {}'.format(1-hamming_loss(y_trues, y_preds)))
 
