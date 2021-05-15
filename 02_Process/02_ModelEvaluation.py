437d72b4a681b855a34db97f38badaebf055eb10
Professor Aang <beck1017@hotmail.com>
2021-05-16 00:59:26 +0800

    02_ModelEvaluation: add Majority Vote Ensemble Classfier (eclf)

--- 02_Process/02_ModelEvaluation.py
+++ 02_Process/02_ModelEvaluation.py
@@ -1,25 +1,24 @@
+# import utililies
 from pandas import read_csv
 from matplotlib import pyplot
 import numpy as np
-
+from statistics import median
+# import model_selection
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import cross_val_score
 from sklearn.model_selection import RepeatedKFold
 from sklearn.model_selection import KFold
-
-from sklearn.metrics import classification_report
-from sklearn.metrics import confusion_matrix
+# import metrics
 from sklearn.metrics import accuracy_score, hamming_loss
-
+# import models
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB, MultinomialNB
-from sklearn.svm import LinearSVC, SVC
-
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+# import multiclass
 from sklearn.multiclass import OneVsRestClassifier
-from statistics import median
 
 # load dataset
 country = ['Country']
@@ -28,9 +27,7 @@ labels = ['L1', 'L2', 'L3', 'L4']
 names = country + features + labels
 filepath = "01_RawData/02_ClassificationTraining_ModelInput.csv"
 df = read_csv(filepath, header=None, skiprows=1, names=names, encoding='utf8')
-
-# split train/test set
-# train, test = train_test_split(df, random_state=42000, test_size=0.20, shuffle=True)
+X = df[features].values
 
 # define models
 models = []
@@ -41,18 +38,33 @@ models.append(('CART', DecisionTreeClassifier(), []))
 models.append(('NB', GaussianNB(), []))
 models.append(('SVM', SVC(gamma='auto'), []))
 
-# define nFolds
+# define ensemble classifier
+eclf_HS_score = []
+
+# define cross validation
 cv = RepeatedKFold(n_splits=10, n_repeats=10, random_state=2652124)
 
-X = df[features].values
-
 # 1. For nFolds
+cnt = 1
 for train_index, test_index in cv.split(X):
+    print('#', cnt)
+    cnt += 1
     # print("TRAIN:", train_index)
     # print("TEST:", test_index)
+    # 
+    pred_sumup = {}
+    shape = df[labels].values[test_index].shape[0]
+    print('shape:', shape)
+    # 
+    pred_sumup['L1'] = np.zeros(shape=(4,1))
+    pred_sumup['L2'] = np.zeros(shape=(4,1))
+    pred_sumup['L3'] = np.zeros(shape=(4,1))
+    pred_sumup['L4'] = np.zeros(shape=(4,1))
+    eclf_preds = []
+    eclf_trues = []  
     # 2. For Models
     for name, model, score in models:
-        # print('model: ' + name)
+        print('model: ' + name)              
         # define OvR strategy
         ovr = OneVsRestClassifier(model)
         # multilable classification
@@ -67,71 +79,34 @@ for train_index, test_index in cv.split(X):
             ovr.fit(X_train, y_train)    
             # make predictions
             y_pred = ovr.predict(X_test)
-            # print('y_pred is {}'.format(y_pred))
+            print('y_pred is {}'.format(y_pred))
+            # pred sum up for eclf
+            pred_sumup[label] = np.add(pred_sumup[label], y_pred)
             y_preds.append(y_pred)
-            # print('y_true is {}'.format(y_test))
+            print('y_true is {}'.format(y_test))
             y_trues.append(y_test)
             # print('Test accuracy is {}'.format(accuracy_score(y_test, y_pred)))
         hamming_score = 1-hamming_loss(y_trues, y_preds)
         score.append(hamming_score)
-        # print('Hamming score (label-based accuracy) is {}'.format(hamming_score))   
-     
-     
-# # ------------------------- backup
-# # train and test each model in turn
-# for name, model in models:    
-#     print('model: ' + name)
-#     # define OvR strategy
-#     ovr = OneVsRestClassifier(model)
-#     # multilable classification
-#     y_trues = []
-#     y_preds = []
-#     for label in labels:
-#         # print('... Processing label: {}'.format(label))
-#         X_train = train[features].values	
-#         y_train = train[label].values
-#         X_test = test[features].values
-#         y_test = test[label].values
-#         # fit model    
-#         ovr.fit(X_train, y_train)    
-#         # make predictions
-#         y_pred = ovr.predict(X_test)
-#         # print('y_pred is {}'.format(y_pred))
-#         y_preds.append(y_pred)
-#         # print('y_true is {}'.format(y_test))
-#         y_trues.append(y_test)
-#         # print('Test accuracy is {}'.format(accuracy_score(y_test, y_pred)))
-
-#     # multilabel evaluation metrics
-#     # print('Subset accuracy (exact match ratio) is {}'.format(accuracy_score(y_trues, y_preds)))
-#     print('Hamming score (label-based accuracy) is {}'.format(1-hamming_loss(y_trues, y_preds)))
-# # -------------------------
-
-
-# evaluate each model in turn
-
-# names = []
-# for name, model in models:
-#     print('model: ' + name)
-#     results = []
-#     # perform sampling for each label
-#     for label in labels:
-#         # print('... Processing label: {}'.format(label))      
-#         X = df[features].values
-#         y = df[label].values
-#         # X_train = train[features].values
-#         # y_train = train[label].values
-#         # X_test = test[features].values
-#         # y_test = test[label].values
-#         cv = RepeatedKFold(n_splits=5, n_repeats=10, random_state=36851234)
-#         cv_results = cross_val_score(model, X, y, cv=cv, scoring='accuracy')
-#         print('%s: %f (%f)' % (label, cv_results.mean(), cv_results.std()))   
-#         results.append(cv_results.mean()) 
+        print('Hamming score (label-based accuracy) is ', '{0:.4f}'.format(hamming_score))  
+        # print('y_trues is {}'.format(y_trues))
+        eclf_trues = y_trues
     
-#     # numpy.array
-#     print('%s: %f' % (name, sum(results) / len(results)))    
-#     names.append(name)
+    # Compute Majority Vote Score  
+    for key in pred_sumup:            
+        for arr in pred_sumup[key]:
+            print('pred_sumup: ', arr)
+            for i in range(len(arr)):
+                if arr[i] <= 3: 
+                    arr[i] = 0
+                else:
+                    arr[i] = 1
+        eclf_preds.append(arr)             
+    eclf_HS = 1-hamming_loss(eclf_trues, eclf_preds)
+    eclf_HS_score.append(eclf_HS) 
+    print('Hamming score of Majority Vote is ', '{0:.4f}'.format(eclf_HS))  
 
+# clf
 model_scores = []
 model_names = []
 for name, model, score in models:
@@ -142,8 +117,17 @@ for name, model, score in models:
           "min:", "{0:.3f}".format(min(score)), 
           "avg:", "{0:.3f}".format(sum(score)/len(score)), 
           "median:", "{0:.3f}".format(median(score)))
+
+# eclf
+model_scores.append(eclf_HS_score) 
+model_names.append('eclf')
+print("model:", 'eclf', "\t", 
+        "max:", "{0:.3f}".format(max(eclf_HS_score)), 
+        "min:", "{0:.3f}".format(min(eclf_HS_score)), 
+        "avg:", "{0:.3f}".format(sum(eclf_HS_score)/len(eclf_HS_score)), 
+        "median:", "{0:.3f}".format(median(eclf_HS_score)))
     
-# Model Evaluation
+# model evaluation
 pyplot.boxplot(model_scores, showmeans=True, labels=model_names)
 pyplot.title('Model Evaluation')
 pyplot.show()
