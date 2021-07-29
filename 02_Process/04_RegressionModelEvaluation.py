a95397f07b505d5aa099741a4ef10e75bc9f5764
Professor Aang <beck1017@hotmail.com>
2021-07-29 21:39:16 +0800

    Six Regression Models & Residual Plots

--- 02_Process/04_RegressionModelEvaluation.py
+++ 02_Process/04_RegressionModelEvaluation.py
@@ -1,29 +1,25 @@
 # import utililies
 from pandas import read_csv
-# from matplotlib import pyplot
 import matplotlib.pyplot as plt
 import numpy as np
-from statistics import median
+
 # import model_selection
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
-from sklearn.model_selection import RepeatedKFold
-from sklearn.model_selection import KFold
-# import metrics
-from sklearn.metrics import accuracy_score, hamming_loss
-from sklearn.metrics import r2_score, mean_squared_error
-# import models
-from sklearn.linear_model import LogisticRegression
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
 
+# import metrics
+from sklearn.metrics import mean_squared_error
+
+# import models
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import VotingRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.tree import DecisionTreeRegressor
+
+# -------------------------------------------
+# Part 1: Model Training and Testing
+# -------------------------------------------
 
 # Define X
 Year = ['Year']
@@ -52,18 +48,26 @@ Subsystems.append(('Energy', Y_Energy))
 Subsystems.append(('Food', Y_Food))
 
 # Define Models
-M_GB = GradientBoostingRegressor(random_state=1)
-M_RF = RandomForestRegressor(random_state=1)
-M_LR = LinearRegression()
-M_Vote = VotingRegressor([('gb', M_GB), ('rf', M_RF), ('lr', M_LR)])
-
 Models = []
+M_GB = GradientBoostingRegressor(random_state=1)
 Models.append(('GradientBoostingRegressor', M_GB))
+
+M_RF = RandomForestRegressor(random_state=1)
 Models.append(('RandomForestRegressor', M_RF))
+
+M_LR = LinearRegression()
 Models.append(('LinearRegression', M_LR))
+
+M_Vote = VotingRegressor([('gb', M_GB), ('rf', M_RF), ('lr', M_LR)])
 Models.append(('VotingRegressor', M_Vote))
 
-# Residual Containers
+M_KNN = KNeighborsRegressor(n_neighbors=3, weights='distance', radius=1)
+Models.append(('KNeighborsRegressor', M_KNN))
+
+M_DT = DecisionTreeRegressor(max_depth=10, random_state=1)
+Models.append(('DecisionTreeRegressor', M_DT))
+
+# Subsystems Metadata Containers
 Residual_Subsystems = []
 Ypred_Subsystems = []
 
@@ -75,11 +79,13 @@ for Y_name, Y in Subsystems:
                                     train_size = 0.8, 
                                     test_size = 0.2, 
                                     shuffle = False)
-  
-  scores = []
+    
+  # Models Metadata Containers  
+  Scores_Models = []
   Residual_Models = []
   Ypred_Models = []
-# For each Model: 
+  
+  # For each Model: 
   for m_name, m in Models: 
     print('m_name:', m_name)
     # Train 
@@ -92,19 +98,19 @@ for Y_name, Y in Subsystems:
     y_train_pred = m.predict(x_train)
     MSE_train = mean_squared_error(y_train, y_train_pred)
     # print('y_train:', y_train)
-    print('y_train_pred:', y_train_pred)
+    # print('y_train_pred:', y_train_pred)
     # print('MSE_train:', "{:.2e}".format(MSE_train))
     
     # Test MSE
     MSE_test = mean_squared_error(y_test, y_test_pred)
     # print('y_test:', y_test)
-    print('y_test_pred:', y_test_pred)
+    # print('y_test_pred:', y_test_pred)
     # print('MSE_test:', "{:.2e}".format(MSE_test))
         
     # Score Append
-    scores.append(MSE_test)
+    Scores_Models.append(MSE_test)
         
-    # Residual  
+    # Residual Computations
     Residual_Train = []  
     zip_object = zip(y_train, y_train_pred)
     for list1_i, list2_i in zip_object:
@@ -126,52 +132,100 @@ for Y_name, Y in Subsystems:
     # print('Residual:', Residual)
      
     # Ypred 
-    # Ypred = y_train_pred + y_test_pred
     Ypred = np.concatenate([y_train_pred, y_test_pred]).tolist()
     Ypred_Models.append(Ypred)
-    print('Ypred:', Ypred)
-    
-    # # print arrays
-    # print('y_test: ')
-    # print(["{0:0.6f}".format(i) for i in y_test])
-    # print('y_test_pred: ')
-    # print(["{0:0.6f}".format(i) for i in y_test_pred])
-    # print('y_train: ')
-    # print(["{0:0.6f}".format(i) for i in y_train])
-    # print('y_train_pred: ')
-    # print(["{0:0.6f}".format(i) for i in y_train_pred])    
+    # print('Ypred:', Ypred)  
 
   # Compare Scores
-  # print(np.argsort(scores), '\n')
+  print('Model Ranking:', np.argsort(Scores_Models), '\n')
   
   Residual_Subsystems.append(Residual_Models)
   Ypred_Subsystems.append(Ypred_Models)
 
 # print('Residual_Subsystems:', Residual_Subsystems)
-print('Ypred_Subsystems:', Ypred_Subsystems)
-# TODO: Need to Refresh the model and then do the next training?
+# print('Ypred_Subsystems:', Ypred_Subsystems)
 
+# -------------------------------------------
+# Part 2: Residual Plot Drawing
+# -------------------------------------------
 
-# Residual Plot 
+# Residual Plot
+plt.figure()
+nrows = len(Models)
+ncols = len(Subsystems)
+Sn = 1
+Mn = 0
+Axs = []
+Labels = ['Water','Energy','Food']
 
+# For each Subsystem: 
+for Sn_Ypred, Sn_Residual in zip(Ypred_Subsystems, Residual_Subsystems): 
+  # Plotting Configurations
+  if Sn==1:
+    color = 'steelblue'
+    marker = 'o'
+    s = 100
+    xmin = 2.1
+    xmax = 2.3
+  elif Sn==2:
+    color = 'orange'
+    marker = '*'
+    s = 200
+    xmin = 2.06
+    xmax = 2.12
+  elif Sn==3: 
+    color = 'limegreen'
+    marker = 's'  
+    s = 80
+    xmin = 1.1
+    xmax = 1.42
+  
+  # For each Model: 
+  for Mn_Ypred, Mn_Residual in zip(Sn_Ypred, Sn_Residual):
+    # Plotting Configurations
+    if Mn==0:
+      title = 'Gradient Boosting'
+    elif Mn==1:
+      title = 'Random Forest'
+    elif Mn==2: 
+      title = 'Linear Regression' 
+    elif Mn==3:
+      title = 'Voting Regressor'
+    elif Mn==4: 
+      title = 'K Nearest Neighbors'
+    elif Mn==5:
+      title = 'Decision Tree Regressor' 
+    
+    # Subplot Indexing and Drawing
+    PltIdx = Mn*ncols + Sn
+    plt.subplot(nrows, ncols, PltIdx)
+    ax = plt.scatter(Mn_Ypred, Mn_Residual, c=color, marker=marker, s=s, edgecolor='white')
+    if Mn==0:
+      Axs.append(ax)
+    plt.xlabel('Predicted values')
+    plt.ylabel('Residuals')
+    plt.gca().set_title(title)
+    plt.gca().set_xlim(xmin=xmin, xmax=xmax)
+    yabs_max = abs(max(plt.gca().get_ylim(), key=abs))
+    plt.gca().set_ylim(ymin=-yabs_max*1.2, ymax=yabs_max*1.2) 
+    plt.hlines(y=0, xmin=xmin, xmax=xmax, color='black', lw=2)
+    
+    # Subplot Indexing 
+    Mn += 1
+  
+  # Subplot Indexing
+  Mn = 0
+  Sn += 1
 
+# Plotting Configurations
+plt.subplots_adjust(top=0.914,
+                    bottom=0.054,
+                    left=0.05,
+                    right=0.987,
+                    hspace=1.0,
+                    wspace=0.214)
+plt.figlegend(handles=Axs, labels=Labels, 
+                    loc='upper center', ncol=len(Labels), prop={'size': 14})
 
-# # Plot the results
-# plt.figure()
-# plt.plot(pred1, 'gd', label='GradientBoostingRegressor')
-# plt.plot(pred2, 'b^', label='RandomForestRegressor')
-# plt.plot(pred3, 'ys', label='LinearRegression')
-# plt.plot(pred4, 'r*', ms=10, label='VotingRegressor')
-
-# plt.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
-# plt.ylabel('predicted')
-# plt.xlabel('training samples')
-# plt.legend(loc="best")
-# plt.title('Regressor predictions and their average')
-# plt.show()
-
-
-
-
-
-
+# Plot the results
+plt.show()
